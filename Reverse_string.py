0c9e352a40939b07a18aad1dab396943d08b8e54
Ashraf <ashrafalict7@gmail.com>
2023-08-25 17:33:28 +0530

    Update Reverse_string.py

--- Reverse_string.py
+++ Reverse_string.py
@@ -1,16 +1,4 @@
-# code_for_reverse_string
-string = input("enter string for reverse : ")
-
-
-def reverse(string):
-    str = " "
-    for x in string:
-        str = x + str
-    return str
-
-
-print("you entered string is : ", string)
-print("reversed string is : ", reverse(string))
-'''result-
-fedcba
-'''
+# sample
+a = 5
+b = 10
+print(a + b)
